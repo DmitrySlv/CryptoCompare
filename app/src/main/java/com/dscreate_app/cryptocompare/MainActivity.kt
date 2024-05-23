45aa47633297bc1943a8edc2d5c0274cf9c609bb
DmitrySlv <dscreateapp@gmail.com>
2024-05-23 18:32:49 +0400

    Добавление заивисмостей в проект.

--- app/src/main/java/com/dscreate_app/cryptocompare/MainActivity.kt
+++ app/src/main/java/com/dscreate_app/cryptocompare/MainActivity.kt
@@ -2,11 +2,14 @@ package com.dscreate_app.cryptocompare
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.dscreate_app.cryptocompare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
